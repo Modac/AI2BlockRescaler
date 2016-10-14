2071e398f9e648bdaa75dbb2b5550debedd65f25
Modac <modac@gmx.net>
2016-10-14 20:46:53 +0200

    Replaced browser_action with page_action

--- src/background.js
+++ src/background.js
@@ -11,3 +11,37 @@ chrome.extension.onConnect.addListener(function(port) {
 		}
   });
 });
+
+/*
+// Called when the url of a tab changes.
+function checkForValidUrl(tabId, changeInfo, tab) {
+	// If the tabs url starts with "http://specificsite.com"...
+	console.log(tab.url)
+	if (tab.url.startsWith('http://ai2.appinventor.mit.edu/')) {
+		console.log("Showing extension")
+		// ... show the page action.
+		chrome.pageAction.show(tabId);
+	}
+};
+
+// Listen for any changes to the URL of any tab.
+chrome.tabs.onUpdated.addListener(checkForValidUrl);
+*/
+
+chrome.runtime.onInstalled.addListener(function() {
+  // Replace all rules ...
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+    // With a new rule ...
+    chrome.declarativeContent.onPageChanged.addRules([
+      {
+        conditions: [
+          new chrome.declarativeContent.PageStateMatcher({
+            pageUrl: { urlPrefix: 'http://ai2.appinventor.mit.edu/' },
+          })
+        ],
+        // And shows the extension's page action.
+        actions: [ new chrome.declarativeContent.ShowPageAction() ]
+      }
+    ]);
+  });
+});
